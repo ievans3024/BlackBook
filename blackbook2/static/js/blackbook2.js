a9050aca3292bf320a540ebe27338c32a8d61c6d
Ian Evans <ianev@ubookstore.com>
2014-07-30 14:46:47 -0700

    * tweaks to get contact list to populate on page load.

--- blackbook2/static/js/blackbook2.js
+++ blackbook2/static/js/blackbook2.js
@@ -33,8 +33,28 @@ angular.module('BlackBook.controllers', []).controller(
         contactsService.getContactList().then(
             function (response) {
                 // Success callback. Extract information and set it in $scope.
-                console.log(response.data);
-                //$scope.contactList = response;
+
+                var contacts = [],
+                contact_items = response.data.collection.items,
+                contact_count = contact_items.length;
+                i = 0,
+                contact = null,
+                contact_item = null,
+                contact_item_length = 0,
+                j = 0;
+
+                for (i; i<contact_count; i++) {
+                    contact = {};
+                    contact_item = contact_items[i].data;
+                    contact_item_length = contact_item.length;
+                    j = 0;
+                    for (j; j<contact_item.length; j++) {
+                        contact[contact_item[j].name] = contact_item[j].value;
+                    }
+                    contacts.push(contact);
+                }
+
+                $scope.contactList = contacts;
             }
         );
     }
