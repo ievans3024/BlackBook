5c104d7d4401ac287eff204d5bfe8a5014d75714
ievans3024 <ievans3024@gmail.com>
2014-08-03 01:24:34 -0700

    * ng twiddling

--- blackbook2/static/js/blackbook2.js
+++ blackbook2/static/js/blackbook2.js
@@ -19,7 +19,7 @@ angular.module('BlackBook.services', []).factory(
 
         contactAPI.getContact = function(href) {
             var headers = { 'Accept': 'application/vnd.collection+json' };
-            return $http.get(href, { headers: headers}).then(function(response){console.log(response.data.collection.items)}); // please note how this might be insecure
+            return $http.get(href, { headers: headers}); // please note how this might be insecure
         };
 
         return contactAPI;
@@ -31,33 +31,42 @@ angular.module('BlackBook.controllers', []).controller(
         $scope.selectedContact = null;
         $scope.contactList = [];
 
-        $scope.getContact = contactsService.getContact;
+        $scope.processCollection = function (response) {
+            var contacts = [],
+            contact_items = response.data.collection.items,
+            contact_count = contact_items.length;
+            i = 0,
+            contact = null,
+            contact_item = null,
+            contact_item_length = 0,
+            j = 0;
+
+            for (i; i<contact_count; i++) {
+                contact = {};
+                contact.href = contact_items[i].href;
+                contact_item = contact_items[i].data;
+                contact_item_length = contact_item.length;
+                j = 0;
+                for (j; j<contact_item.length; j++) {
+                    contact[contact_item[j].name] = contact_item[j].value;
+                }
+                contacts.push(contact);
+            }
+
+            return contacts;
+        };
+
+        $scope.getContact = function(href) {
+            contactsService.getContact(href).then(
+            function(response) {
+                $scope.selectedContact = $scope.processCollection(response);
+            }
+            );
+        };
+
         contactsService.getContactList().then(
             function (response) {
-                // Success callback. Extract information and set it in $scope.
-
-                var contacts = [],
-                contact_items = response.data.collection.items,
-                contact_count = contact_items.length;
-                i = 0,
-                contact = null,
-                contact_item = null,
-                contact_item_length = 0,
-                j = 0;
-
-                for (i; i<contact_count; i++) {
-                    contact = {};
-                    contact.href = contact_items[i].href;
-                    contact_item = contact_items[i].data;
-                    contact_item_length = contact_item.length;
-                    j = 0;
-                    for (j; j<contact_item.length; j++) {
-                        contact[contact_item[j].name] = contact_item[j].value;
-                    }
-                    contacts.push(contact);
-                }
-
-                $scope.contactList = contacts;
+                $scope.contactList = $scope.processCollection(response);
             }
         );
     }
