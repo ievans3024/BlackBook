df1ec8d31cf5e06e153b48a3e84031a536e6d707
ievans3024 <ievans3024@gmail.com>
2014-11-22 20:42:59 -0800

    * moved some js files * broke out angular app into several files for minification

---
+++ blackbook/static/js/src/blackbook-services.js
@@ -0,0 +1,23 @@
+black_book.services = black_book.services || angular.module('BlackBook.services', []);
+
+black_book.services.factory(
+    'contacts_service', function($http) {
+
+        return {
+            create: function (data) {
+
+            },
+            read: function(href) {
+                var headers = { 'Accept': 'application/vnd.collection+json' };
+                return $http.get(href, { headers: headers }); // please note how this might be insecure
+            },
+            update: function (href, data) {
+
+            },
+            delete: function(href) {
+                return $http.delete(href);
+            }
+        }
+
+    }
+);
---
+++ blackbook/static/js/src/blackbook.js
@@ -0,0 +1,24 @@
+var black_book = angular.module('BlackBook', [
+    'BlackBook.controllers',
+    'BlackBook.filters',
+    'BlackBook.services'
+],
+function($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+});
+
+black_book.filters = angular.module('BlackBook.filters', []);
+
+black_book.filters.filter(
+    'capitalize', function () {
+        return function (input) {
+            var string_array = input.split(' '),
+                i;
+            for (i = 0; i < string_array.length; i++) {
+                string_array[i] = string_array[i][0].toUpperCase() + string_array[i].slice(1, string_array[i].length);
+            }
+            return string_array.join(' ');
+        };
+    }
+);
