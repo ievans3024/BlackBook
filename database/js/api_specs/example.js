4d9ce9361a91831a5c20ea1d32d249556a2fa5e9
Ian Evans <ianev@ubookstore.com>
2015-05-13 11:07:45 -0700

    + added example database javascript builder files

--- database/js/api_specs/example.js
+++ database/js/api_specs/example.js
@@ -1,5 +1,5 @@
 /**
- * Created by ian on 5/13/15.
+ * Created by ievans3024 on 5/13/15.
  */
 
 exports.spec = function (dbname) {
--- database/js/build.js
+++ database/js/build.js
@@ -1,5 +1,5 @@
 /**
- * Created by ian on 5/13/15.
+ * Created by ievans3024 on 5/13/15.
  */
 
 var emit = function(){},  // so node doesn't bitch about emit not existing
@@ -13,10 +13,10 @@ var emit = function(){},  // so node doesn't bitch about emit not existing
     filesystem = require("fs"),
     schema = [
         // Design Docs
-        // require("./design_docs/docname.js").design
+        // require("./design_docs/example.js").design
 
         // API Specs
-        // require("./api_specs/specname.js").spec(dbname)
+        // require("./api_specs/example.js").spec(dbname)
     ];
 
 schema_string = JSON.stringify(schema, replacer);
