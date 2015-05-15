b2a7c76ed746f718c55d01c83256fed6a6f4a72b
Ian Evans <ianev@ubookstore.com>
2015-05-15 10:12:50 -0700

    + added some options - removed old options

--- blackbook/config.py
+++ blackbook/config.py
@@ -1,6 +1,14 @@
-TESTING = True
-DATABASE_HANDLER = 'sqlite'
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/blackbook/test.db'
-WHOOSH_BASE = '/tmp/blackbook/search.db'
-MAX_SEARCH_RESULTS = 50
+from datetime import timedelta
+
+# Flask Builtin Options
+DEBUG = True
+TESTING = False
+PERMANENT_SESSION_LIFETIME = timedelta(days=14)
+SECRET_KEY = """Make this really super secret and random!"""
+SERVER_NAME = None  # address and port, default is 127.0.0.1:5000
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = False  # True in https production
+
+# Custom Options
+COUCHDB_URI = "http://localhost:5984"
 API_ROOT = '/api/'
