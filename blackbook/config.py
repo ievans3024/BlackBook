e1fc23ab65afc4f2cca573973ae6d0fc33b25e6e
ievans3024 <ievans3024@gmail.com>
2015-05-17 00:58:02 -0700

    + added pagination setting to config

--- blackbook/config.py
+++ blackbook/config.py
@@ -13,3 +13,4 @@ SESSION_COOKIE_SECURE = False  # True in https production
 PUBLIC_REGISTRATION = False  # Set to True to allow anyone to make an account
 COUCHDB_URI = "http://localhost:5984"
 API_ROOT = '/api/'
+API_PAGINATION_PER_PAGE = 10
