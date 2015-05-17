c50df9342999c2bf59c3a54a5bc497edc97154e6
ievans3024 <ievans3024@gmail.com>
2015-05-17 02:48:03 -0700

    + added code to handle database plugins

--- blackbook/app.py
+++ blackbook/app.py
@@ -1,10 +1,21 @@
 __author__ = 'ievans3024'
 
 from flask import Flask, render_template
+from importlib import import_module
 
 app = Flask('blackbook')
 app.config.from_pyfile('config.py', silent=True)
 
+try:
+    api = import_module("api", package=app.config.get("DATABASE_HANDLER"))
+except AttributeError:
+    raise ValueError("Config option 'DATABASE_HANDLER' is an invalid value or does not exist.")
+except ValueError:
+    raise ValueError("Config option 'DATABASE_HANDLER' cannot be empty.")
+except ImportError:
+    raise ValueError("Config option 'DATABASE_HANDLER' is {value} but there is no module to support that".format(value=app.config.get("DATABASE_HANDLER")))
+
+app.register_blueprint(api)
 
 @app.route("/")
 @app.route("/book/")
