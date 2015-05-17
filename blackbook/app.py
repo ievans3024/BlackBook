00c75aa0a41ff2acd3008eea5769ed1151371ebd
ievans3024 <ievans3024@gmail.com>
2015-05-17 02:54:02 -0700

    * changed database plugin handler code to work with how importlib.import_module works

--- blackbook/app.py
+++ blackbook/app.py
@@ -7,7 +7,7 @@ app = Flask('blackbook')
 app.config.from_pyfile('config.py', silent=True)
 
 try:
-    api = import_module("api", package=app.config.get("DATABASE_HANDLER"))
+    plugin = import_module(app.config.get("DATABASE_HANDLER"))
 except AttributeError:
     raise ValueError("Config option 'DATABASE_HANDLER' is an invalid value or does not exist.")
 except ValueError:
@@ -15,7 +15,7 @@ except ValueError:
 except ImportError:
     raise ValueError("Config option 'DATABASE_HANDLER' is {value} but there is no module to support that".format(value=app.config.get("DATABASE_HANDLER")))
 
-app.register_blueprint(api)
+app.register_blueprint(plugin.api)
 
 @app.route("/")
 @app.route("/book/")
