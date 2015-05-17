788720fa894355971e5f6f1d209ee67e62638a23
ievans3024 <ievans3024@gmail.com>
2015-05-17 02:47:16 -0700

    + added config option DATABASE_HANDLER

--- blackbook/config.py
+++ blackbook/config.py
@@ -11,6 +11,7 @@ SESSION_COOKIE_SECURE = False  # True in https production
 
 # Custom Options
 PUBLIC_REGISTRATION = False  # Set to True to allow anyone to make an account
+DATABASE_HANDLER = "couchdb"
 COUCHDB_URI = "http://localhost:5984"
 API_ROOT = '/api/'
 API_PAGINATION_PER_PAGE = 10
