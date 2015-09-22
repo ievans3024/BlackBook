8577ee8f0fbd60bf4c43915ad3e5255c15c35b5f
Ian Evans <ianev@ubookstore.com>
2015-09-22 10:45:15 -0700

    * updated imports

--- blackbook/couch/database.py
+++ blackbook/couch/database.py
@@ -1,31 +1,32 @@
-__author__ = 'ievans3024'
+import couchdb
+import couchdb.mapping
+import json
+import os.path
 
-from couchdb import ResourceNotFound, Server
-from couchdb.mapping import Document
-from json import load
-from os.path import abspath, dirname, join
+__author__ = 'ievans3024'
 
 
 def init_db(app):
     dbname = app.config.get("DB_NAME") or "blackbook"
-    server = Server(app.config.get("COUCHDB_URI") or "http://localhost:5984")  # default to couch default uri
+    server = couchdb.Server(app.config.get("COUCHDB_URI") or "http://localhost:5984")  # default to couch default uri
 
     # Create database if it doesn't exist
     try:
         db = server[dbname]
-    except ResourceNotFound:
+    except couchdb.ResourceNotFound:
         db = server.create(dbname)
 
     # Load in schema file
-    with open(join(join(dirname(abspath(__file__)), "js"), "{db}.json".format(db=dbname))) as schema_file:
-        schema = load(schema_file)
+    schema_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "{0}.json".format(dbname))
+    with open(schema_path) as schema_file:
+        schema = json.load(schema_file)
 
     # Update design docs and api specs according to schema file,
     # create new docs if they don't exist
     for doc in schema:
-        d = Document.load(db, doc.get("_id"))
+        d = couchdb.mapping.Document.load(db, doc.get("_id"))
         if not d:
-            d = Document(id=doc.get("_id"))
+            d = couchdb.mapping.Document(id=doc.get("_id"))
         for k, v in doc.items():
             if k in d:
                 if d[k] != v:
