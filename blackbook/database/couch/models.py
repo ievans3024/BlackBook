fa4fbb328c215bf81dd20ac3bd1c9614b9df383e
Ian Evans <ianev@ubookstore.com>
2015-10-19 16:37:17 -0700

    + added Session couch model - removed old couch models

--- blackbook/database/couch/models.py
+++ blackbook/database/couch/models.py
@@ -1,7 +1,6 @@
 import blackbook.database
 import couchdb.mapping
 import datetime
-import re
 import uuid
 
 from flask import current_app
@@ -10,6 +9,8 @@ __author__ = 'ievans3024'
 
 # TODO: full-text search with couch-lucene (and pylucene?)
 #   see: https://wiki.apache.org/couch/Full_text_search
+
+LOGIN_TIMEOUT = current_app.config.get('LOGIN_TIMEOUT') or {'days': 14}
 
 
 class CouchModel(couchdb.mapping.Document):
@@ -86,6 +87,14 @@ class Group(Permissible):
     description = couchdb.mapping.TextField()
 
 
+class Session(CouchModel):
+    user = couchdb.mapping.TextField()
+    token = couchdb.mapping.TextField()
+    expiry = couchdb.mapping.DateTimeField(
+        default=lambda: datetime.datetime.now() + datetime.datetime.timedelta(**LOGIN_TIMEOUT)
+    )
+
+
 class User(Permissible):
 
     email = couchdb.mapping.TextField()
@@ -95,141 +104,3 @@ class User(Permissible):
     email_verified = couchdb.mapping.BooleanField()
     active = couchdb.mapping.BooleanField()
     last_active = couchdb.mapping.DateTimeField()
-
-
-class Contact(BaseDocument):
-    """A contact being stored in the "book"."""
-    user = couchdb.mapping.TextField()  # references User.id
-    name_first = couchdb.mapping.TextField()
-    name_last = couchdb.mapping.TextField()
-    date_created = couchdb.mapping.DateTimeField(default=datetime.datetime.now)
-    date_modified = couchdb.mapping.DateTimeField(default=datetime.datetime.now)
-    defaults = couchdb.mapping.DictField(
-        couchdb.mapping.Mapping.build(
-            address=couchdb.mapping.DictField(
-                couchdb.mapping.Mapping.build(
-                    label=couchdb.mapping.TextField(),
-                    line_1=couchdb.mapping.TextField(),
-                    line_2=couchdb.mapping.TextField(),
-                    city=couchdb.mapping.TextField(),
-                    state=couchdb.mapping.TextField(),
-                    zip=couchdb.mapping.TextField(),
-                    country=couchdb.mapping.TextField()
-                )
-            ),
-            email=couchdb.mapping.DictField(
-                couchdb.mapping.Mapping.build(
-                    label=couchdb.mapping.TextField(),
-                    email=couchdb.mapping.TextField()
-                )
-            ),
-            phone_number=couchdb.mapping.DictField(
-                couchdb.mapping.Mapping.build(
-                    label=couchdb.mapping.TextField(),
-                    number=couchdb.mapping.TextField()
-                )
-            )
-        )
-    )
-    addresses = couchdb.mapping.ListField(
-        couchdb.mapping.DictField(
-            couchdb.mapping.Mapping.build(
-                label=couchdb.mapping.TextField(),
-                line_1=couchdb.mapping.TextField(),
-                line_2=couchdb.mapping.TextField(),
-                city=couchdb.mapping.TextField(),
-                state=couchdb.mapping.TextField(),
-                zip=couchdb.mapping.TextField(),
-                country=couchdb.mapping.TextField()
-            )
-        )
-    )
-    emails = couchdb.mapping.ListField(
-        couchdb.mapping.DictField(
-            couchdb.mapping.Mapping.build(
-                label=couchdb.mapping.TextField(),
-                email=couchdb.mapping.TextField()
-            )
-        )
-    )
-    phone_numbers = couchdb.mapping.ListField(
-        couchdb.mapping.DictField(
-            couchdb.mapping.Mapping.build(
-                label=couchdb.mapping.TextField(),
-                number=couchdb.mapping.TextField()
-            )
-        )
-    )
-
-    @property
-    def name(self):
-        return " ".join([self.name_first, self.name_last])
-
-    @property
-    def rname(self):
-        return ", ".join([self.name_last, self.name_first])
-
-    def get_collection_items(self):
-        pass
-
-
-class Group(Permissible):
-    """A group for users of the system."""
-
-    name = couchdb.mapping.TextField()
-    description = couchdb.mapping.TextField()
-    by_name = couchdb.mapping.ViewField("group", "")
-
-    def get_collection_items(self):
-        pass
-
-
-class Session(BaseDocument):
-    """Session data for Users"""
-
-    token = couchdb.mapping.TextField(default=uuid.uuid4())
-    user = couchdb.mapping.TextField()  # references User.id
-    expiry = couchdb.mapping.DateTimeField(
-        default=lambda: datetime.datetime.now() + current_app.config.get("PERMANENT_SESSION_LIFETIME") or datetime.timedelta(days=14)
-    )
-    by_token = couchdb.mapping.ViewField("session", "")
-    by_user = couchdb.mapping.ViewField("session", "")
-
-    def get_collection_items(self):
-        pass
-
-
-class User(Permissible):
-    """A user of the system."""
-
-    password_hash = couchdb.mapping.TextField()
-    name = couchdb.mapping.TextField()
-    email = couchdb.mapping.TextField()
-    contacts = couchdb.mapping.ListField(couchdb.mapping.TextField())
-    by_name = couchdb.mapping.ViewField("user", "")
-    by_email = couchdb.mapping.ViewField("user", "")
-    by_salt = couchdb.mapping.ViewField("user", "")
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def get_collection_items(self):
-        pass
-
-    def set_email(self, db, email):
-        users = self.by_email(db, key=email)
-        if not users.rows:
-            self.email = email
-        else:
-            raise blackbook.database.ModelError('A user with that email already exists.')
-
-    def set_password(self, db, password):
-        hash_method = current_app.config.get('PASSWORD_HASH_METHOD') or 'pbkdf2:sha512'
-        salt_length = current_app.config.get('PASSWORD_SALT_LENGTH') or 12
-        while True:
-            new_hash = generate_password_hash(password, method=hash_method, salt_length=salt_length)
-            salt = new_hash.split("$")[1]
-            users = self.by_salt(db, key=salt)
-            if not users.rows:
-                self.password_hash = new_hash
-                break
