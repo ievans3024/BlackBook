7ea73a62a574dfa742b88b9d573aec2bdb0e5aa9
Ian Evans <ianev@ubookstore.com>
2017-03-30 11:20:50 -0700

    Added config documentation and config defaults

--- blackbook/app.py
+++ blackbook/app.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from blackbook.api import ContactAPI, SessionAPI, UserAPI
+from datetime import timedelta
 
 __author__ = 'ievans3024'
 
 app = Flask('blackbook')
+
+app.config.setdefault('PERMANENT_SESSION_LIFETIME', timedelta(days=14))
+app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+app.config.setdefault('PASSWORD_HASH_METHOD', 'pbkdf2:sha512')
+app.config.setdefault('PASSWORD_SALT_LENGTH', 12)
+app.config.setdefault('API_URL_ROOT', '/api/')
+app.config.setdefault('API_PAGINATION_PER_PAGE', 10)
+
 app.config.from_pyfile('config.py', silent=True)
+
 app.db = SQLAlchemy(app)
 
-app.add_url_rule('/api/contact/', view_func=ContactAPI.as_view('contacts', app, app.db, '/api/contact/'))
-app.add_url_rule('/api/contact/<contact_id>/', view_func=ContactAPI.as_view('contact', app, app.db, '/api/contact/'))
-app.add_url_rule('/api/session/', view_func=SessionAPI.as_view('sessions', app, app.db, '/api/session/'))
-app.add_url_rule('/api/user/', view_func=UserAPI.as_view('users', app, app.db, '/api/user/'))
-app.add_url_rule('/api/user/<user_id>/', view_func=UserAPI.as_view('user', app, app.db, '/api/user/'))
+app.add_url_rule(app.config.get('API_URL_ROOT') + 'contact/',
+                 view_func=ContactAPI.as_view('contacts', app, app.db, '/api/contact/'))
+app.add_url_rule(app.config.get('API_URL_ROOT') + 'contact/<contact_id>/',
+                 view_func=ContactAPI.as_view('contact', app, app.db, '/api/contact/'))
+app.add_url_rule(app.config.get('API_URL_ROOT') + 'session/',
+                 view_func=SessionAPI.as_view('sessions', app, app.db, '/api/session/'))
+app.add_url_rule(app.config.get('API_URL_ROOT') + 'user/',
+                 view_func=UserAPI.as_view('users', app, app.db, '/api/user/'))
+app.add_url_rule(app.config.get('API_URL_ROOT') + 'user/<user_id>/',
+                 view_func=UserAPI.as_view('user', app, app.db, '/api/user/'))
 
 
 @app.route("/")
--- blackbook/config.py
+++ blackbook/config.py
@@ -1,21 +1,33 @@
-from datetime import timedelta
-
 # Flask Builtin Options
+# see http://flask.pocoo.org/docs/config/#builtin-configuration-values
 DEBUG = True
 TESTING = False
-PERMANENT_SESSION_LIFETIME = timedelta(days=14)
 SECRET_KEY = """Make this really super secret and random!"""
 SERVER_NAME = None  # address and port, default is 127.0.0.1:5000
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = False  # True in https production
 
 # Flask-SQLAlchemy Options
+# see http://flask-sqlalchemy.pocoo.org/config/
 SQLALCHEMY_DATABASE_URI = ''
-SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Custom Options
-PASSWORD_HASH_METHOD = 'pbkdf2:sha512'  # only methods supported by functions in werkzueg.security
+# PASSWORD_HASH_METHOD
+#   A string that will be passed to werkzueg.security functions
+#   to specify which hashing algorithm to use
+# PASSWORD_SALT_LENGTH
+#   An integer indicating length of generated password salts
+# PUBLIC_REGISTRATION
+#   A boolean indicating whether to allow open public registration
+#   or admin-created accounts only
+# API_ROOT
+#   A string indicating the api root. Must end in a forward slash ('/')
+# API_PAGINATION_PER_PAGE
+#   An integer indicating maximum results per "page" in a response
+#   from API routes.
+
+PASSWORD_HASH_METHOD = 'pbkdf2:sha512'
 PASSWORD_SALT_LENGTH = 12
-PUBLIC_REGISTRATION = False  # Set to True to allow anyone to make an account
+PUBLIC_REGISTRATION = False
 API_ROOT = '/api/'
 API_PAGINATION_PER_PAGE = 10
