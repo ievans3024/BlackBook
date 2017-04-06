99defc660132bc20366dc9fecf051bf9b17f2c8c
Ian Evans <ievans3024@gmail.com>
2017-04-06 16:39:21 -0700

    Fiddling with setup.py requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ import blackbook
 from setuptools import setup
 
 setup(
-    name="BlackBook",
+    name='BlackBook',
     version=blackbook.__version__,
-    url="https://github.com/ievans3024/BlackBook",
-    license="MIT",
+    url='https://github.com/ievans3024/BlackBook',
+    license='MIT',
     author=blackbook.__author__,
-    description="A contact management application",
-    test_suite="tests.test_all",
-    install_requires=['flask', 'flask_sqlalchemy']
+    description='A contact management application',
+    test_suite='tests.test_all',
+    python_requires='>=3.0',
+    install_requires=['flask >=0.12', 'flask_sqlalchemy >=2.1']
 )
